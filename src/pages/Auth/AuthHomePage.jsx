e05395fd312284fdf65350e3cf2c9924249bedd9
adetayoalexander <adetayoalexander12@gmail.com>
2024-04-25 00:26:42 +0100

    Designed stories section

--- src/pages/Auth/AuthHomePage.jsx
+++ src/pages/Auth/AuthHomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AuthHomePage.module.css";
 import { Link } from "react-router-dom";
 import { NavHashLink, HashLink } from "react-router-hash-link";
@@ -15,7 +15,14 @@ import Main_content from "../../assets/images/main_content.jpg";
 import Main_tribe from "../../assets/images/main_tribe.jpg";
 import Main_influence from "../../assets/images/main_influence.jpg";
 import Main_analytics from "../../assets/images/main_analytics.jpg";
-import {motion, AnimatePresence } from "framer-motion";
+import DPM1 from "../../assets/images/dpM1.jpg";
+import DPM2 from "../../assets/images/dpM2.jpg";
+import DPM3 from "../../assets/images/dpM3.jpg";
+import DPM4 from "../../assets/images/dpM4.jpg";
+import DPM5 from "../../assets/images/dpM5.jpg";
+import DPF1 from "../../assets/images/dpF1.jpg";
+import DPF2 from "../../assets/images/dpF2.jpg";
+import { motion, AnimatePresence } from "framer-motion";
 
 const AuthHomePage = () => {
   const Links = [
@@ -55,7 +62,7 @@ const AuthHomePage = () => {
     {
       id: 0,
       header: "Stand Out, Shine Bright",
-      desc: "Create captivating, compelling and distinctive content that's distinctly yours",
+      desc: "Create captivating and distinctive content that's distinctly yours",
       main_img: Main_content,
       flat_img: Flat_content,
     },
@@ -64,7 +71,7 @@ const AuthHomePage = () => {
       header: "Uncover Your Tribe",
       desc: "Explore diverse communities, uncover new passions, and connect with like-minded individuals",
       main_img: Main_tribe,
-      flat_img: Flat_tribe, 
+      flat_img: Flat_tribe,
     },
     {
       id: 2,
@@ -79,6 +86,65 @@ const AuthHomePage = () => {
       desc: "Gain valuable insights with advanced analytics for data-driven growth and optimization",
       main_img: Main_analytics,
       flat_img: Flat_analytics,
+    },
+  ];
+
+  const stories = [
+    {
+      id: 0,
+      name: "John Doe",
+      username: "geedoe00",
+      avatar: DPM1,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 1,
+      name: "John Doe",
+      username: "geedoe00",
+      avatar: DPM2,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      username: "baddiejane00",
+      avatar: DPF1,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      username: "geedoe00",
+      avatar: DPM3,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 4,
+      name: "Jane Doe",
+      username: "baddiejane00",
+      avatar: DPF2,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 5,
+      name: "John Doe",
+      username: "geedoe00",
+      avatar: DPM4,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
+    },
+    {
+      id: 6,
+      name: "John Doe",
+      username: "geedoe00",
+      avatar: DPM5,
+      testimony:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet et massa venenatis molestie. Pellentesque facilisis lacinia nunc id suscipit. Curabitur laoreet, lorem sit amet facilisis dignissim, magna lacus tristique orci, ullamcorper vehicula est sem at leo. Phasellus id dapibus sapien.",
     },
   ];
 
@@ -108,6 +174,9 @@ const AuthHomePage = () => {
     return Math.abs(offset) * velocity;
   };
 
+  const [activeId, setActiveId] = useState(3);
+  const [newStories, setNewStories] = useState([]);
+
   const [[page, direction], setPage] = useState([0, 0]);
 
   const paginate = (newDirection) => {
@@ -120,6 +189,21 @@ const AuthHomePage = () => {
     }
   };
 
+  useEffect(() => {
+    const activeStoryIndex = stories.findIndex(
+      (story) => story.id === activeId
+    );
+    const remainingStoryIndexes = stories.filter(
+      (story) => story.id !== activeId
+    );
+    const reorderedStories = [
+      ...remainingStoryIndexes.slice(0, 3),
+      stories[activeStoryIndex],
+      ...remainingStoryIndexes.slice(3)
+    ];
+    setNewStories(reorderedStories);
+  }, [activeId]);
+
   return (
     <main className={classes["main"]}>
       <section className={`${classes["section"]} ${classes["home"]}`}>
@@ -131,7 +215,7 @@ const AuthHomePage = () => {
               lobortis.
             </p>
             <NavHashLink className={classes["action"]} to={"/auth#footer"}>
-              Continue ►
+              Continue
             </NavHashLink>
           </div>
           <img src={Hero} alt="hero image" />
@@ -245,15 +329,15 @@ const AuthHomePage = () => {
                   src={item.main_img}
                   alt="main-img"
                 />
-                  <motion.img
-                    className={classes["flat-img"]}
-                    src={item.flat_img}
-                    alt="flat-img"
-                  />
-                  <motion.article>
-                    <h3>{item.header}</h3>
-                    <p>{item.desc}</p>
-                  </motion.article>
+                <motion.img
+                  className={classes["flat-img"]}
+                  src={item.flat_img}
+                  alt="flat-img"
+                />
+                <motion.article>
+                  <h3>{item.header}</h3>
+                  <p>{item.desc}</p>
+                </motion.article>
               </motion.div>
             ))}
             <div className={classes["next"]} onClick={() => paginate(1)}>
@@ -262,14 +346,17 @@ const AuthHomePage = () => {
                 version="1.1"
                 x="0px"
                 y="0px"
-                width='50px'
-                height='50px'
+                width="40px"
+                height="40px"
                 viewBox="0 0 100 125"
                 style={{ enableBackground: "new 0 0 100 100" }}
               >
                 <switch>
                   <g>
-                    <path fill="var(--text-1)" d="M97.5,50C97.5,23.8,76.2,2.5,50,2.5S2.5,23.8,2.5,50c0,26.2,21.3,47.5,47.5,47.5S97.5,76.2,97.5,50z M50.2,68.7L36.1,54.5    c-1.2-1.2-1.9-2.9-1.9-4.5c0-1.6,0.6-3.3,1.9-4.5l14.2-14.2c2.5-2.5,6.5-2.5,9,0c2.5,2.5,2.5,6.5,0,9L49.6,50l9.7,9.7    c2.5,2.5,2.5,6.5,0,9C56.8,71.2,52.7,71.2,50.2,68.7z" />
+                    <path
+                      fill="var(--text-1)"
+                      d="M97.5,50C97.5,23.8,76.2,2.5,50,2.5S2.5,23.8,2.5,50c0,26.2,21.3,47.5,47.5,47.5S97.5,76.2,97.5,50z M50.2,68.7L36.1,54.5    c-1.2-1.2-1.9-2.9-1.9-4.5c0-1.6,0.6-3.3,1.9-4.5l14.2-14.2c2.5-2.5,6.5-2.5,9,0c2.5,2.5,2.5,6.5,0,9L49.6,50l9.7,9.7    c2.5,2.5,2.5,6.5,0,9C56.8,71.2,52.7,71.2,50.2,68.7z"
+                    />
                   </g>
                 </switch>
               </svg>
@@ -280,14 +367,17 @@ const AuthHomePage = () => {
                 version="1.1"
                 x="0px"
                 y="0px"
-                width='50px'
-                height='50px'
+                width="40px"
+                height="40px"
                 viewBox="0 0 100 125"
                 style={{ enableBackground: "new 0 0 100 100" }}
               >
                 <switch>
                   <g>
-                    <path fill="var(--text-1)" d="M97.5,50C97.5,23.8,76.2,2.5,50,2.5S2.5,23.8,2.5,50c0,26.2,21.3,47.5,47.5,47.5S97.5,76.2,97.5,50z M50.2,68.7L36.1,54.5    c-1.2-1.2-1.9-2.9-1.9-4.5c0-1.6,0.6-3.3,1.9-4.5l14.2-14.2c2.5-2.5,6.5-2.5,9,0c2.5,2.5,2.5,6.5,0,9L49.6,50l9.7,9.7    c2.5,2.5,2.5,6.5,0,9C56.8,71.2,52.7,71.2,50.2,68.7z" />
+                    <path
+                      fill="var(--text-1)"
+                      d="M97.5,50C97.5,23.8,76.2,2.5,50,2.5S2.5,23.8,2.5,50c0,26.2,21.3,47.5,47.5,47.5S97.5,76.2,97.5,50z M50.2,68.7L36.1,54.5    c-1.2-1.2-1.9-2.9-1.9-4.5c0-1.6,0.6-3.3,1.9-4.5l14.2-14.2c2.5-2.5,6.5-2.5,9,0c2.5,2.5,2.5,6.5,0,9L49.6,50l9.7,9.7    c2.5,2.5,2.5,6.5,0,9C56.8,71.2,52.7,71.2,50.2,68.7z"
+                    />
                   </g>
                 </switch>
               </svg>
@@ -297,6 +387,10 @@ const AuthHomePage = () => {
         <div className={classes["navigation"]}>
           {discoveries.map((item) => (
             <button
+              key={item.id}
+              onClick={() => {
+                setPage([item.id, 0]);
+              }}
               className={`${page === item.id && classes.active}`}
             ></button>
           ))}
@@ -309,19 +403,26 @@ const AuthHomePage = () => {
       >
         <h1>Stories</h1>
         <div className={classes["avatars"]}>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          {newStories.map((elem) => (
+            <img
+              onClick={() => {
+                setActiveId(elem.id);
+              }}
+              className={`${activeId === elem.id && classes["active"]}`}
+              src={elem.avatar}
+              alt={`user${elem.id}avatar`}
+            />
+          ))}
         </div>
 
         <article>
           <span className={classes["names"]}>
-            <p>Jane Doe</p>
-            <p>@jane_doe</p>
+            <p>{stories[activeId].name}</p>
+            <p>•</p>
+            <p>@{stories[activeId].username}</p>
           </span>
           <span className={classes["testimony"]}>
-            It is fklkf jflj skjhk kjrf jklfjs syaf epk ugeu fywe otho oihot
-            owerou.
+            {stories[activeId].testimony}
           </span>
         </article>
       </section>
